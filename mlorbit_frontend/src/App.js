f9b3ab23d39f6affc1e9e10c7d1bdde70988f394
Ayushi <minesquad7768@gmail.com>
2024-12-16 22:25:21 +0530

    Update App.js

--- mlorbit_frontend/src/App.js
+++ mlorbit_frontend/src/App.js
@@ -6,11 +6,11 @@ import WelcomeSection from "./components/WelcomeSection";
 import Footer from "./components/Footer";
 import AboutSection from "./components/AboutSection";
 import RecommendedSections from "./components/RecommendedSections";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import Algorithms from "./pages/Algorithms";
 import DataStructures from "./pages/DataStructures";
 import MachineLearning from "./pages/MachineLearning";
-import LoginPage from "./login/LoginPage";
+
 function App() {
   return (
     <Router>
@@ -24,14 +24,13 @@ function App() {
               <Sections />
               <RecommendedSections />
               <Footer />
-              <AboutSection />
             </>
           }
         />
+        <Route path="/about" element={<AboutSection />} />
         <Route path="/algorithms" element={<Algorithms />} />
         <Route path="/data-structures" element={<DataStructures />} />
         <Route path="/machine-learning" element={<MachineLearning />} />
-        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
